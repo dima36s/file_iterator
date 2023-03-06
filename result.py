6d6e7401fbe9d9e4c1408151d0d41b8f44baca26
Dmitry Samoylov <76055016+dima36s@users.noreply.github.com>
2023-03-06 11:55:04 +0300

    Update result.py

--- result.py
+++ result.py
@@ -1,23 +1,33 @@
 import os
 
 
-class FilesystemIterator:
-    def __init__(self, root_dir):
-        self.root_dir = os.path.abspath(root_dir)
-
-    def iterate_files(self, directory):
-        for entry in os.listdir(directory):
-            full_path = os.path.join(directory, entry)
-            if os.path.isfile(full_path):
-                yield full_path
-            elif os.path.isdir(full_path):
-                yield from self.iterate_files(full_path)
+class FileIterator:
+    def __init__(self, root, only_files=False, only_dirs=False, pattern=None):
+        self.root = root
+        self.only_files = only_files
+        self.only_dirs = only_dirs
+        self.pattern = pattern
 
     def __iter__(self):
-        return self.iterate_files(self.root_dir)
+        return self.file_iter(self.root)
+
+    def file_iter(self, root):
+        for filename in os.listdir(root):
+            path = os.path.join(root, filename)
+            if os.path.isdir(path):
+                if not self.only_files and (not self.only_dirs or self.only_dirs and self._match_pattern(filename)):
+                    yield path
+                    yield from self.file_iter(path)
+            elif not self.only_dirs and (not self.only_files or self.only_files and self._match_pattern(filename)):
+                yield path
+
+    def _match_pattern(self, filename):
+        if self.pattern:
+            return self.pattern in filename
+        else:
+            return os.path.isfile(os.path.join(self.root, filename))
 
 
 n = input('Вставте свой путь: ')
-file_iter = FilesystemIterator(n)
-for i in file_iter:
-    print(i)
+for filename in FileIterator(n):
+    print(filename)
